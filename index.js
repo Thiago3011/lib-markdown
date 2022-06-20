7acaa0657c968531dc668d0f11b51f999421788b
Thiago3011 <thiago.silva1001@etec.sp.gov.br>
2022-06-20 19:21:12 -0300

    fs, throw new error, async and await

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const chalk = require('chalk');
+const chalk = require('chalk')
+const fs = require('fs')
 
-console.log(chalk.blue('vamos começar!'))
-
-const paragrafo = 'Texto retornado por uma função'
-
-function texto(string) {
-    return string
+const trataErro = (erro) => {
+    throw new Error(chalk.red(erro.code, '- NÃO HÁ ARQUIVO NO CAMINHO'))
 }
 
-console.log(texto(paragrafo))
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, 'utf-8')
+        console.log(chalk.green(texto))
+    } catch (erro) {
+        trataErro(erro)
+    } finally {
+        console.log(chalk.yellow('operação concluída'));
+    }
+}
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     fs.promises.readFile(caminhoDoArquivo, 'utf-8')
+//         .then( (texto) =>console.log(chalk.green(texto)))
+//         .catch( (erro) => trataErro(erro))
+// }
+
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     fs.readFile(caminhoDoArquivo, 'utf-8', (erro, texto) => {
+
+//         if (erro) {
+//             trataErro(erro)
+//         }
+
+//         console.log(chalk.green(texto))
+//     })
+// }
+
+pegaArquivo('./arquivos/texto1.md')
